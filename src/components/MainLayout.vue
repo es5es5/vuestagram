09d4f795b7fd625ae3a4a6f2c6e3e2c867e9984a
es5es5 <es5e5s@nate.com>
2021-09-12 18:29:17 +0900

    datas

--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="app">
     <div class="container">
-      <div class="button-wrapper">
+      <!-- <div class="button-wrapper">
         <button>dd</button>
         <button class="btn" @click="searchUnsplash('Autumn')">Autumn</button>
-      </div>
+      </div> -->
       <stack
-              :column-min-width="300"
-              :gutter-width="15"
-              :gutter-height="15"
-              monitor-images-loaded
+        :column-min-width="300"
+        :gutter-width="15"
+        :gutter-height="15"
+        monitor-images-loaded
       >
         <stack-item
-                v-for="(image, i) in images"
-                :key="i"
-                style="transition: transform 300ms"
+          v-for="(image, i) in images"
+          :key="i"
+          style="transition: transform 300ms"
         >
           <img :src="image.urls.small" :alt="image.alt_description" />
         </stack-item>
@@ -27,6 +27,7 @@
 import Vue from 'vue'
 import axios from 'axios'
 import { Stack, StackItem } from 'vue-stack-grid'
+import autumn from '@/assets/data/autumn.json'
 
 export default Vue.extend({
   name: 'Main',
@@ -36,7 +37,7 @@ export default Vue.extend({
   },
   data () {
     return {
-      images: [],
+      images: autumn,
       // client_id: 'qPZTUxnvzIcggSG3yugWxx45eX4x5dqrJlnGoeCEMbs',
       client_id: '',
       items: [{
@@ -63,18 +64,4 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-.container {
-    width: 80vw;
-    margin: 0 auto;
-}
-.button-wrapper {
-    display: flex;
-    justify-content: center;
-    margin-bottom: 25px;
-}
-.btn {
-    font-size: 18px;
-    background-color: #42b983;
-    color: white; padding: 10px 20px;
-}
 </style>
