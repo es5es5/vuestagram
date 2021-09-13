4a0417341c392167618b9a032a34647be9b30c45
es5es5 <es5e5s@nate.com>
2021-09-13 23:13:41 +0900

    Images

---
+++ src/components/Images.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul class="images">
+    <li v-for="(image, index) in images" :key="index">
+      <img :src="image.urls.small" :alt="image.alt_description" class="image" />
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import datas from '@/assets/data/couples.json'
+import axios from 'axios'
+
+export default Vue.extend({
+  name: 'Images',
+  data () {
+    return {
+      images: datas,
+      search: 'spring',
+      client_id: 'qPZTUxnvzIcggSG3yugWxx45eX4x5dqrJlnGoeCEMbs',
+    }
+  },
+  methods: {
+    searchUnsplash () {
+      this.images = []
+      axios.get(`https://api.unsplash.com/search/photos?client_id=${this.client_id}&query=${this.search}&per_page=30`, {
+      }).then(response => {
+        this.images = response.data.results
+      }).catch(() => {
+        this.images = []
+      })
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.image {
+  display: inline-block;
+  vertical-align: middle;
+  width: 100%;
+}
+</style>
--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -1,43 +1,21 @@
 <template>
   <div id="app">
     <Header />
-    <ul class="images">
-      <li v-for="(image, index) in images" :key="index">
-        <img :src="image.urls.small" :alt="image.alt_description" class="image" />
-      </li>
-    </ul>
+    <Images />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Header from '@/components/Header.vue'
-import axios from 'axios'
-import datas from '@/assets/data/couples.json'
+import Images from './Images.vue'
 
 export default Vue.extend({
   name: 'Main',
   components: {
     Header,
+    Images,
   },
-  data () {
-    return {
-      images: datas,
-      search: 'spring',
-      client_id: 'qPZTUxnvzIcggSG3yugWxx45eX4x5dqrJlnGoeCEMbs',
-    }
-  },
-  methods: {
-    searchUnsplash () {
-      this.images = []
-      axios.get(`https://api.unsplash.com/search/photos?client_id=${this.client_id}&query=${this.search}&per_page=30`, {
-      }).then(response => {
-        this.images = response.data.results
-      }).catch(() => {
-        this.images = []
-      })
-    }
-  }
 })
 </script>
 
